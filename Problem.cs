c72fc89295eeafaa3cd3f8c7e7e34c4fba0357b8
Konrad8181 <128095042+Konrad8181@users.noreply.github.com>
2023-06-05 02:07:48 -0700

    Problem solver

---
+++ Problem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksProblem
+{
+    public static class Problem
+    {
+        public static int MaxPages(int n, int x, int[] c, int[] s)
+        {
+            int[,] dp = new int[n+1,x+1];
+
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j <= x; j++)
+                {
+                    if (i== 0 || j == 0)
+                    {
+                        dp[i, j] = 0;
+                    }
+                    else if (c[i - 1] <= j)
+                    {
+                        dp[i, j] = Math.Max(s[i - 1] + dp[i-1,j - c[i-1]], dp[i-1,j]);
+                    }
+                    else
+                    {
+                        dp[i, j] = dp[i - 1, j];
+                    }
+                }
+            }
+            return dp[n, x];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+using BooksProblem;
+
+const int MaxN = 1000;
+const int MaxX = 100000;
+const int MaxCiSi = 1000;
+
+var r = new Random();
+
+int n = r.Next(1, MaxN);
+int x = r.Next(1, MaxX);
+
+int[] c = new int[n];
+int[] s = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    c[i] = r.Next(1, MaxCiSi);
+    s[i] = r.Next(1, MaxCiSi);
+}
+
+int maxPages = Problem.MaxPages(n, x, c, s);
+Console.WriteLine("Maximum number of pages: " + maxPages);
